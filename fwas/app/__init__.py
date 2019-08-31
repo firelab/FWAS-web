8e447d857142d02016e4cea2f99db69827065495
levi malott <levi@malott.co>
2019-08-31 06:55:02 -0500

    Configured logging for flask app.

--- fwas/app/__init__.py
+++ fwas/app/__init__.py
@@ -6,9 +6,17 @@ from .api import blueprint as api_blueprint
 from .config import Config
 
 
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+
 def create_app(config=Config):
+
+    logger.info('Creating app..')
     app = Flask(__name__)
 
     app.register_blueprint(api_blueprint, url_prefix="/api")
+
+    logger.info('App created')
 
     return app
